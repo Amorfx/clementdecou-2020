6507c447a72f67935e41484f3f9f7b939294c0c5
Clément Décou <clementdecou@macbook-pro-de-clement.home>
2020-12-13 00:50:35 +0100

    feat(projects-home): add filter isotope

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,9 +1,23 @@
 const Isotope = require('isotope-layout');
+const $ = require('jquery');
 
-const grid = document.querySelector('.projects');
-var iso = new Isotope(grid, {
-  itemSelector: '.project',
-  masonry: {
-    columnWidth: '.grid-sizer'
-  }
+$(document).ready(function(){
+  const grid = document.querySelector('.projects');
+  var iso = new Isotope(grid, {
+    itemSelector: '.project',
+    masonry: {
+      columnWidth: '.grid-sizer'
+    }
+  });
+
+  // Filter for isotope
+  $('.projects--filters span[data-filter]').on('click', function(event) {
+    event.preventDefault();
+    const $filterClicked = $(this);
+    const filterValue = $filterClicked.data('filter');
+    $('.projects--filters li span').removeClass('active');
+    $filterClicked.addClass('active');
+    iso.arrange({ filter: filterValue });
+  });
 });
+
