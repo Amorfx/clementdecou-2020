9f284ac89273763214d03700847c1630b505b70a
Clément Décou <clementdecou@macbook-pro-de-clement.home>
2020-12-20 14:46:41 +0100

    refactor(get-menu): refactor to have global class ThemeUtils in all twig template

--- functions.php
+++ functions.php
@@ -1,4 +1,8 @@
 <?php
+
+use ClementCore\Admin\ThemeSettings;
+use Theme2020\Utils\ThemeUtils;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 // Add theme supports
@@ -12,7 +16,8 @@ add_filter('simply_views_directory', function($viewsDirectory) {
 });
 
 add_filter('simply_template_configuration', function (\Twig\Environment $twig) {
-    $twig->addFunction(new \Twig\TwigFunction('isLive', \ClementCore\Admin\ThemeSettings::class . '::isLive'));
+    $twig->addFunction(new \Twig\TwigFunction('isLive', ThemeSettings::class . '::isLive'));
+    $twig->addGlobal('theme', new ThemeUtils());
     return $twig;
 });
 
--- src/Utils/ThemeUtils.php
+++ src/Utils/ThemeUtils.php
@@ -2,12 +2,24 @@
 
 namespace Theme2020\Utils;
 
+use ClementCore\Admin\ThemeMenu;
+use Theme2020\Models\Menu\Menu;
+
 class ThemeUtils {
-    static function getStylesheets($name) {
+    public function getStylesheets($name) {
         return get_template_directory_uri() . '/dist/css/' . $name;
     }
 
-    static function getJavascript($name) {
+    public function getJavascript($name) {
         return get_template_directory_uri() . '/dist/js/' . $name;
+    }
+
+    private function getMenuItems($location) {
+        $menu = new Menu($location);
+        return $menu->getItems();
+    }
+
+    public function getMenuHeaderItems() {
+        return $this->getMenuItems(ThemeMenu::$headerNavSlug);
     }
 }
