0200eedce890016d711814f8ea11569d11bd08d8
Clément Décou <clementdecou@macbook-pro-de-clement.home>
2021-01-30 23:31:01 +0100

    feat(blog): add display name author, link and date in blog card

--- assets/js/utils/header.js
+++ assets/js/utils/header.js
@@ -3,6 +3,9 @@ const $document = $(document);
 
 module.exports.animDarkHeader = () => {
   // Have menu dark or white with scroll
+  if ($document.scrollTop() >= 16) {
+    $('header').first().removeClass('dark');
+  }
   $document.scroll((e) => {
     const scrollTop = $document.scrollTop();
     if (scrollTop <= 16) {
